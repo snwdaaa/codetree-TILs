450bf44c08fbd446de4661d8822b2c82143faa05
SeungWoo Lee <20211922@edu.hanbat.ac.kr>
2024-09-20 17:13:25 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240920/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Total days in Year" << '\n' << 365 << '\n' << "Circumference rate" << '\n';
+    cout.precision(13);
+    cout << 3.1415926535;
+    return 0;
+}
