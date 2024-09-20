da8b48cae688ec753ccdd8ce7ac626d4b9ce46b7
SeungWoo Lee <20211922@edu.hanbat.ac.kr>
2024-09-20 17:49:20 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240920/두 사람/two-person.cpp
+++ 240920/두 사람/two-person.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     int age[2];
     char gender[2];
-    cin >> age[0] >> age[1];
-    cin >> gender[0] >> gender[1];
+    cin >> age[0] >> gender[0];
+    cin >> age[1] >> gender[1];
 
     bool cond1 = age[0] >= 19 || gender[0] == 'M';
     bool cond2 = age[1] >= 19 || gender[1] == 'M';
