b641fcbf9a5456c3f8835f1c5b1f261220e85081
SeungWoo Lee <20211922@edu.hanbat.ac.kr>
2024-09-20 17:16:15 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240920/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a = 5.26;
+    float b = 8.27;
+
+    cout << fixed;
+    cout.precision(3);
+    cout << a*b;
+
+    return 0;
+}
