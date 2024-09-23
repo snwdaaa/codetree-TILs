cdffb51c21e4aea47f79f308ed10c5d13a51ae43
SeungWoo Lee <20211922@edu.hanbat.ac.kr>
2024-09-23 13:30:22 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240923/1까지 나누기/divide-by-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    cin >> n;
+
+    int i = 1;
+    int cnt = 0;
+    while (n >= 1)
+    {
+        n /= i;
+        i++;
+    }
+
+    cout << i - 1;
+
+    return 0;
+}
